6d0da4477dd0f1b234df331ea63c665196826790
Keynaffz <keynafz29@gmail.com>
2026-05-19 11:15:18 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace App\Controllers;
+use App\Models\ProductModel;
 
 class Home extends BaseController
 {
-    public function index(): string
+    protected $productModel;
+    public function __construct()
     {
-        return view('v_home');
+        $this->productModel = new ProductModel();
+    }
+
+    public function index()
+    {
+        $products = $this->productModel->findAll();
+        $data['products'] = $products;
+
+        return view('v_home', $data);
     }
 }
